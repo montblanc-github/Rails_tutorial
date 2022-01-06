f8f0678d0c0dc25f152d6f7797407dbbceeab66b
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-06 08:21:59 +0000

    :construction: goodbyeアクション追加
    goodbyeアクションを追加したため

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  def hello
-    render html: "¡Hola, mundo!"
+  # def hello
+    # render html: "¡Hola, mundo!"
+  # end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 
 end
