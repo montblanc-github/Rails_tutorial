55957a2e8a24b2d7fd6325fad8231d81a6209805
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-07 09:18:04 +0000

    :memo: topページ文章変更
    topページ文章を変更したため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
   # def hello
-    # render html: "¡Hola, mundo!"
+  #   render html: "¡Hola, mundo!"
   # end
 
   def goodbye
-    render html: "goodbye, world!"
+    render html: "¡Hola, mundo!"
   end
 
 end
